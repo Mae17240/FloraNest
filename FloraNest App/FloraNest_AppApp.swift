d3e85c1d43e90592c16b31bded2dd1cf44a440ff
Mae17240 <joshuamae01@gmail.com>
2024-11-08 18:58:41 +0000

    Initial Commit

---
+++ FloraNest App/FloraNest_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloraNest_AppApp.swift
+//  FloraNest App
+//
+//  Created by Joshua Mae on 08/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FloraNest_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FloraNest AppTests/FloraNest_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FloraNest_AppTests.swift
+//  FloraNest AppTests
+//
+//  Created by Joshua Mae on 08/11/2024.
+//
+
+import Testing
+@testable import FloraNest_App
+
+struct FloraNest_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
