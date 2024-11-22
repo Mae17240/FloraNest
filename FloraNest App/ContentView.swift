12ccd80d1d5a311619426e63f6efa9e400eb0433
Mae17240 <joshuamae01@gmail.com>
2024-11-22 19:19:12 +0000

    Background image. Image import.

--- FloraNest App/ContentView.swift
+++ FloraNest App/ContentView.swift
@@ -9,16 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geo in
+            ZStack {
+                Image("FNbackground")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 500, height: 1000)
+                    .edgesIgnoringSafeArea(.all)
+                
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
