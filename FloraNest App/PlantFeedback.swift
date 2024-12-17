c2e18f629070c62323b8ece6782e1f0df3c32c37
Mae17240 <joshuamae01@gmail.com>
2024-12-17 15:46:02 +0000

    Pages + images import

--- FloraNest App/PlantFeedback.swift
+++ FloraNest App/PlantFeedback.swift
@@ -1,18 +1,62 @@
+//
+//  PlantFeedback.swift
+//  FloraNest App
+//
+//  Created by Joshua Mae on 12/12/2024.
+//
 import SwiftUI
 
 struct PlantFeedback: View {
     var body: some View {
-        ZStack {
-            // Background Image
-            Image("FNbackground")
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
+        NavigationView {
+            
+            ZStack {
+                //Background Image
+                Image("MyPlants")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                
+                //my plants spaced text
+                VStack {
+                    HStack {
+                        VStack(alignment: .leading, spacing: -5)  { //(header vertical spacing!)
+                            Text("Plant Info.")
+                                .font(Font.custom("Fraunces", size: 40))
+                                .foregroundColor(.white)
+                                .fontWeight(.semibold)
+                            
+                            Text("About This Plant.")
+                                .font(Font.custom("Poppins-regular", size: 20))
+                                .foregroundColor(.white)
+                                .fontWeight(.semibold)
+                        }
+                        .padding(.leading, 130)
+                        Spacer()
+                    }
+                    Spacer()
+                    
+                    //bottom menu-bar to hold my Button(images)
+                    RoundedRectangle(cornerRadius: 60)
+                        .fill(Color.black.opacity(0))
+                        .frame(height: 60)
+                        .frame(width: 350)
+                        .overlay(
+                            
+                            HStack(spacing: 200){
+                                NavigationLink(destination: ContentView()){
+                                    Image("HomeBtn")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 57, height: 57)
+                                }
+                            }
+                        )
+                }
+            }
         }
     }
 }
-
-
 
 struct PlantFeedback_Previews: PreviewProvider {
     static var previews: some View {
